8d566cbc1dafabd7d516d5668d95384acc6553f1
Ukrany <ukrany@gmail.com>
2014-10-20 19:04:04 +0100

    Reformatação de path

--- java/main/src/com/isel/adeetc/leic/si/serie1/ex7/model/JWTValidator.java
+++
@@ -1,9 +0,0 @@
-package com.isel.adeetc.leic.si.serie1.ex7.model;
-
-public class JWTValidator {
-
-	public static boolean validate(JOSE_HeaderS jOSE_header, String payload,String signature) {
-		
-		return false;
-	}
-}
